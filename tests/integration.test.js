7a1e19d26e25e76223f47a55de0af0412d30b65d
myndzi <krisreeves@searchfanatics.com>
2013-11-27 13:47:01 -0800

    Add a 'then' method to client.send that will chain further routing keys to be passed the result of the current one. Optionally specify headers, and automatically include the routingKey the message was passed from.
    FIXME: things are getting messy, refactor

--- tests/integration.test.js
+++ tests/integration.test.js
@@ -16,28 +16,43 @@ describe('Integration tests', function () {
 		});
 		
 		var batcher = new ArchEnemy.Batcher(function (batch) {
-			this.resolve();
-		}, { maxSize: 1 });
+			batch[0].deferred.resolve('kittens');
+			return 'batch result';
+		}, { execTimeout: 100, maxSize: 1 });
 		
 
 		client.consume({
 			'bar': [ 'foo' ]
 		}, function (msg) {
 			msg.should.equal('message');
-			return batcher.add(msg).then(function () {
+			return batcher.add(msg).then(function (res) {
 				client.send('keke', 'success');
+				return res;
 			});
 		});
 
+		var closing = false;
 		client.consume({
 			'keke': [ 'keke' ]
 		}, function (msg) {
 			msg.should.equal('success');
 		});
+		
+		client.consume({
+			'unf': [ 'unf' ]
+		}, function (msg) {
+			msg.should.equal('kittens');
+			this.headers.fromKey.should.equal('foo');
+			this.headers['content-type'].should.equal('test message');
+			
+			if (closing) return;
+			closing = true;
+			setTimeout(function () {
+				client.close().then(done.bind(null, null));
+			}, 100);
+		});
 
-		setTimeout(function () {
-			client.close().then(done.bind(null, null));
-		}, 1500);
-		client.send('foo', 'message');
+		client.send('foo', 'message')
+			.then('unf', { 'content-type': 'test message' });
 	});
 });
