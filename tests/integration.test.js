2e0e4c82094f97e8f5852197d47119e3b3799ad7
myndzi <krisreeves@searchfanatics.com>
2013-11-27 12:26:45 -0800

    Get graceful client closure working Add a live test sequence

--- tests/integration.test.js
+++ tests/integration.test.js
@@ -7,13 +7,35 @@ var when = require('when'),
 	ArchEnemy = require('..');
 
 describe('Integration tests', function () {
-	var client = new ArchEnemy.Client({
-		connection: {
-			url: config.amqp,
-			params: { heartbeat: 10 }
-		}
-	});
-	it('should wait a bit', function (done) {
-		client.close().ensure(done);
+	it('should not suck', function (done) {
+		var client = new ArchEnemy.Client({
+			connection: {
+				url: config.amqp,
+				params: { heartbeat: 10 }
+			}
+		});
+		
+		var batcher = new ArchEnemy.Batcher(function (batch) {
+			this.resolve();
+		}, { maxSize: 1 });
+		
+
+		client.consume({
+			'bar': [ 'foo' ]
+		}, function (msg) {
+			msg.should.equal('message');
+			return batcher.add(msg).then(function () {
+				client.send('keke', 'success');
+			});
+		});
+
+		client.consume({
+			'keke': [ 'keke' ]
+		}, function (msg) {
+			msg.should.equal('success');
+			client.close().then(done.bind(null, null));
+		});
+
+		client.send('foo', 'message');
 	});
 });
